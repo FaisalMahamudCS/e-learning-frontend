ae1fba347cb733246d966d1e711179070d9b926a
Faisal Mahamud <97041463+FaisalMahamudCS@users.noreply.github.com>
2024-05-19 22:08:07 +0600

    Home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,191 @@
+// import Home from "../app/src/";
 import Image from "next/image";
+import { Inter } from 'next/font/google'
+import profile1 from  '/public/profile1.png'
+import catagory1 from  '/public/Icon.png'
+import course1 from  '/public/course1.png'
+import { CiStar } from "react-icons/ci";
+import { IoIosStar } from "react-icons/io";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+<div className="bg-[#FFFFFF]">
+  <div className="flex justify-between m-[25px] gap-6">
+    <div className="flex flex-col gap-6 ">
+  <h2 className="text-[40px] text-gray-900 font-bold font-Inter ">Unlock Your Potential with Byway</h2>
+<h2>Welcome to Byway, where learning knows no bounds. We believe that education is the key to personal and professional growth, and we 're here to guide you on your journey to success. Whether you're a student, professional, or lifelong learner, our cutting-edge Learning Management System is designed to elevate your learning experience.</h2>
+<div>
+<button className="bg-[#2563EB] w-[255px] h-[51px] text-white">
+Start your instruction today
+</button>
+ 
+</div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+</div>
+<Image src={profile1} />  
+</div>
+<div className="p-[80px] bg-[#F8FAFC] ">
+  <div className="flex justify-between">
+   <div>
+    <p>250+ Courses</p>
+    <p>By Best Mentors</p>
+   </div>
+   <div>
+    <p>250+ Courses</p>
+    <p>By Best Mentors</p>
+   </div>
+   <div>
+    <p>250+ Courses</p>
+    <p>By Best Mentors</p>
+   </div>
+  </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+</div>
+<div className="px-[80.5px] py-[80.5px]">
+<h2 className="m-[10px] font-bold">Top Catagories</h2>
+<div className="flex justify-between  gap-[24px]">
+  <div className="px-[93px] py-[24px]  box-border border border-[#E2E8F0] rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <div className=" rounded-[100px] bg-[#E0F2FE] h-[100px] w-[100px] ">
+      <div className="flex  items-center   justify-center h-[100px] w-[100px]">
+<Image src={catagory1} className=" "/>
+</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+</div>
+<p>Astrology</p>
+<p>11 Courses</p>
+  </div>
+  <div className="px-[93px] py-[24px]  box-border border border-[#E2E8F0] rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <div className=" rounded-[100px] bg-[#E0F2FE] h-[100px] w-[100px] ">
+      <div className="flex  items-center   justify-center h-[100px] w-[100px]">
+<Image src={catagory1} className=" "/>
+</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+</div>
+<p>Astrology</p>
+<p>11 Courses</p>
+  </div>
+  <div className="px-[93px] py-[24px]  box-border border border-[#E2E8F0] rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <div className=" rounded-[100px] bg-[#E0F2FE] h-[100px] w-[100px] ">
+      <div className="flex  items-center   justify-center h-[100px] w-[100px]">
+<Image src={catagory1} className=" "/>
+</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+</div>
+<p>Astrology</p>
+<p>11 Courses</p>
+  </div>
+  <div className="px-[93px] py-[24px]  box-border border border-[#E2E8F0] rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <div className=" rounded-[100px] bg-[#E0F2FE] h-[100px] w-[100px] ">
+      <div className="flex  items-center   justify-center h-[100px] w-[100px]">
+<Image src={catagory1} className=" "/>
+</div>
+
+</div>
+<p>Astrology</p>
+<p>11 Courses</p>
+  </div>
+
+</div>
+</div>
+<div className="px-[80.5px] py-[80.5px]">
+<h2 className="m-[10px] font-bold">Top Courses</h2>
+<div className="flex justify-between gap-[24px]">
+<div className="flex justify-between ">
+  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <Image src={course1}/>
+    <p className="font-bold">Beginner Guide to design</p>
+<p>By Ronald Richards</p>
+<div className="flex justify-between">
+<div className="flex justify-between items-center">
+{/* <CiStar /> */}
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<p>120 Reviews</p>
+</div>
+</div>
+<p>22 Total Hours. 155 Lectures. Beginner</p>
+<p  className="font-bold">$149.9</p>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+
+</div>
+<div className="flex justify-between ">
+  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <Image src={course1}/>
+    <p className="font-bold">Beginner Guide to design</p>
+<p>By Ronald Richards</p>
+<div className="flex justify-between items-center">
+{/* <CiStar /> */}
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<p>120 Reviews</p>
+</div>
+<p>22 Total Hours. 155 Lectures. Beginner</p>
+<p  className="font-bold">$149.9</p>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+
+</div>
+<div className="flex justify-between ">
+  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <Image src={course1}/>
+    <p className="font-bold">Beginner Guide to design</p>
+<p>By Ronald Richards</p>
+<div className="flex justify-between">
+<div className="flex justify-between items-center">
+{/* <CiStar /> */}
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<p>120 Reviews</p>
+</div>
+</div>
+<p>22 Total Hours. 155 Lectures. Beginner</p>
+<p  className="font-bold">$149.9</p>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+
+</div>
+<div className="flex justify-between ">
+  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
+    <Image src={course1}/>
+    <p className="font-bold">Beginner Guide to design</p>
+<p>By Ronald Richards</p>
+<div className="flex justify-between">
+<div className="flex justify-between items-center">
+{/* <CiStar /> */}
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<IoIosStar  className="text-[#EAB308]"/>
+<p>120 Reviews</p>
+</div>
+</div>
+<p>22 Total Hours. 155 Lectures. Beginner</p>
+<p  className="font-bold">$149.9</p>
+  </div>
+  <div></div>
+  <div></div>
+  <div></div>
+
+</div>
+</div>
+</div>
+</div> 
+ );
 }
