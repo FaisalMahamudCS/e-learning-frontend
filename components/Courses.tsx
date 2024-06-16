dd4771eac35d6fbba141457aead4a8c1d958c5a9
Faisal Mahamud <97041463+FaisalMahamudCS@users.noreply.github.com>
2024-06-16 12:59:02 +0600

    Axios query added

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -2,8 +2,27 @@ import Image from 'next/image';
 import React from 'react';
 import { IoIosStar } from 'react-icons/io';
 import course1 from  '/public/course1.png'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Courses = () => {
+   async function getCourse(){
+    type User = {
+      id: number;
+      name: string;
+      email: string;
+    };
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/course/all`);
+  const users = (await res.data) ;
+  return users;
+  }
+
+    const { data } = useQuery({
+    queryKey: ['initial-users'],
+    queryFn: () => getCourse(),
+
+    staleTime: 5 * 1000,
+  });
     return (
         <div>
       <div className="flex justify-between gap-[24px]">
@@ -11,6 +30,7 @@ const Courses = () => {
   <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
     <Image src={course1}/>
     <p className="font-bold">Beginner Guide to design</p>
+    
 <p>By Ronald Richards</p>
 <div className="flex justify-between">
 <div className="flex justify-between items-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,13 @@ import Button from "../../components/Home/Button";
 import Searchbar from "../../components/Searchbar";
 import Navbar from "../../components/Navbar";
 import Category from "../../components/Category";
+import { useQuery } from "react-query";
+import Course from "../../components/Course";
+import Courses from "../../components/Courses";
 
 export default function Home() {
+
+
   return (
 <div className="bg-[#FFFFFF]">
   <Navbar/>
@@ -104,102 +109,8 @@ Start your instruction today
 <div className="px-[80.5px] py-[80.5px]">
 <h2 className="m-[10px] font-bold">Top Courses</h2>
 {/* <Category/> */}
-<div className="flex justify-between gap-[24px]">
-<div className="flex justify-between ">
-  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
-    <Image src={course1}/>
-    <p className="font-bold">Beginner Guide to design</p>
-<p>By Ronald Richards</p>
-<div className="flex justify-between">
-<div className="flex justify-between items-center">
-{/* <CiStar /> */}
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<p>120 Reviews</p>
-</div>
-</div>
-<p>22 Total Hours. 155 Lectures. Beginner</p>
-<p  className="font-bold">$149.9</p>
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
+<Courses/>
 
-</div>
-<div className="flex justify-between ">
-  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
-    <Image src={course1}/>
-    <p className="font-bold">Beginner Guide to design</p>
-<p>By Ronald Richards</p>
-<div className="flex justify-between items-center">
-{/* <CiStar /> */}
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<p>120 Reviews</p>
-</div>
-<p>22 Total Hours. 155 Lectures. Beginner</p>
-<p  className="font-bold">$149.9</p>
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
-
-</div>
-<div className="flex justify-between ">
-  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
-    <Image src={course1}/>
-    <p className="font-bold">Beginner Guide to design</p>
-<p>By Ronald Richards</p>
-<div className="flex justify-between">
-<div className="flex justify-between items-center">
-{/* <CiStar /> */}
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<p>120 Reviews</p>
-</div>
-</div>
-<p>22 Total Hours. 155 Lectures. Beginner</p>
-<p  className="font-bold">$149.9</p>
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
-
-</div>
-<div className="flex justify-between ">
-  <div className="flex flex-col gap-[6px] p-4 box-border border border-gray-200 rounded-[16px] shadow-[0_0_8px_0_rgba(59,130,246,0.12)] bg-white">
-    <Image src={course1}/>
-    <p className="font-bold">Beginner Guide to design</p>
-<p>By Ronald Richards</p>
-<div className="flex justify-between">
-<div className="flex justify-between items-center">
-{/* <CiStar /> */}
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<IoIosStar  className="text-[#EAB308]"/>
-<p>120 Reviews</p>
-</div>
-</div>
-<p>22 Total Hours. 155 Lectures. Beginner</p>
-<p  className="font-bold">$149.9</p>
-  </div>
-  <div></div>
-  <div></div>
-  <div></div>
-
-</div>
-</div>
 </div>
 <div className="px-[80.5px] py-[80.5px] ">
 <h1 className="font-bold mb-[10px]">Top Instructor</h1>
