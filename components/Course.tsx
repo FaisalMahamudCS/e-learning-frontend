51cbefccb11506e47ebb7a12fc88ac96b3893354
FaisalMahamudCS <fmfahimcse@gmail.com>
2024-06-16 22:36:27 +0600

    detail added

--- components/Course.tsx
+++ components/Course.tsx
@@ -47,7 +47,7 @@ const Course = () => {
 <IoIosStar  className="text-[#EAB308]"/>
 <IoIosStar  className="text-[#EAB308]"/>
 <p>120 Reviews</p>
-<p>22 Total Hours. 155 Lectures. All levels</p>
+<p>{data?.duration}.{data?.lectures} Lectures. {data?.level} levels</p>
 </div>
 <div className='flex gap-2 items-center'>
 <Image src={ellipse5}/>
@@ -70,14 +70,14 @@ const Course = () => {
         <div className='pl-[80px] '>
             <div className='w-3/4'>
       <p className='font-semibold'>Course Description</p>  
-      <p>This interactive e-learning course will introduce you to User Experience (UX) design, the art of creating products and services that are intuitive, enjoyable, and user-friendly. Gain a solid foundation in UX principles and learn to apply them in real-world scenarios through engaging modules and interactive exercises. </p>
+      <p>{data?.details} </p>
       <p className='font-semibold mt-[24px]'>Certification</p>
       <p>At Byway, we understand the significance of formal recognition for your hard work and dedication to continuous learning. Upon successful completion of our courses, you will earn a prestigious certification that not only validates your expertise but also opens doors to new opportunities in your chosen field.</p>
       <p className='font-semibold mt-[24px]'>Instructor</p>
-      <p>Ronald Richards</p>
+      <p>{data?.instructor}</p>
       <div className='flex justify-between w-[271px] gap-[18px]'>
 <div>
-    <Image src={ellipse4}/>
+    <Image src={data?.photo}/>
     
 </div>
 <div className='w-[135px] pt-[18px]'>
